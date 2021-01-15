4bc24d69bce4983f82e327ca84b9dbb3906c4332
Mitea Dan <mitea_dan@yahoo.com>
2021-01-14 22:02:47 -0500

    Add project files.

---
+++ Logic/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public interface IInput
+{
+    bool Active { get; set; }
+}
+
+public enum Direction { Up, Down }
+
+public class Logic
+{
+    readonly IInput[] inputs;
+    readonly Action<int /*counter*/, Direction, double /*freq in Hz*/> finished;
+
+    public Logic(Action<int, Direction, double> finished)
+    {
+        this.finished = finished;
+    }
+}
---
+++ Simulator/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulator.ViewModels
+{
+    class MainViewModel : ReactiveObject
+    {
+        readonly Logic logic;
+
+        int counter;
+        public int Counter { get => counter; set => this.RaiseAndSetIfChanged(ref counter, value); }
+
+        Direction direction;
+        public Direction Direction { get => direction; set => this.RaiseAndSetIfChanged(ref direction, value); }
+
+        double frequency;
+        public double Frequency { get => frequency; set => this.RaiseAndSetIfChanged(ref frequency, value); }
+
+        public MainViewModel()
+        {
+            logic = new((counter, direction, frequency) => (Counter, Direction, Frequency) = (counter, direction, frequency));
+        }
+    }
+}
